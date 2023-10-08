226a1e7692d2bbd9a89e54e8108f514da3acb761
Maddheshiya007 <chandanmaddhesiya4@gmail.com>
2023-10-09 00:11:27 +0530

    city not found fixed

--- script.js
+++ script.js
@@ -9,29 +9,35 @@ async function checkWeather(city) {
     const response = await fetch(apiurl + city + `&appid=${apikey}`);
     var data = await response.json();
 
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather-data").style.display = "none";
+    }
+    else{
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "images/clear.png";
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "images/rain.png";
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "images/drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "images/mist.png";
+        }
+    
+        document.querySelector(".weather-data").style.display = "flex";
+    }
+
     console.log(data);
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = data.main.temp;
     document.querySelector(".humidity").innerHTML = data.main.humidity;
     document.querySelector(".wind").innerHTML = data.wind.speed;
-
-    if(data.weather[0].main == "Clouds"){
-        weatherIcon.src = "images/clouds.png";
-    }
-    else if(data.weather[0].main == "Clear"){
-        weatherIcon.src = "images/clear.png";
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatherIcon.src = "images/rain.png";
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src = "images/drizzle.png";
-    }
-    else if(data.weather[0].main == "Mist"){
-        weatherIcon.src = "images/mist.png";
-    }
-
-    document.querySelector(".weather-data").style.display = "flex";
 }
 
 searchBtn.addEventListener("click", ()=>{
